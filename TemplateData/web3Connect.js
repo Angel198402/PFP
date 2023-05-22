09279c91fb6d9e84d4bc68c50a298ef7204a59e1
angel <jamesppthompson@gmail.com>
2023-05-22 23:22:23 +0300

    upload to github

---
+++ TemplateData/web3Connect.js
@@ -0,0 +1,39 @@
+
+if (window.ethereum) {
+  web3 = new Web3(window.ethereum);
+  // connect popup
+
+  // window.ethereum.on("accountsChanged", function () {
+  //   location.reload();
+  // });
+
+}
+else {
+  alert("Metamask not detected!, Plesae connect to a wallet")
+}
+
+
+//Query Ethereum blockchain for NFT data  
+async function getNftData() { 
+  // const test = "https://cors-anywhere.herokuapp.com/https://nft.bueno.art/api/contract/bunqwsPH1QYnRyTK2x9SH/chain/1/metadata/30";    
+  //   const test1 = await fetch(test);
+  //   const rmsi = await test1.json();
+  //   console.log(rmsi);  
+    
+    
+  const nftURLs = [];
+  const contract = new web3.eth.Contract(contractABI, contractAddress);  
+  const tokens = await contract.methods.tokensOfOwner(web3.currentProvider.selectedAddress).call();
+  console.log("tokens", tokens);
+  for (let i=0; i<tokens.length; i++){
+    const metadataURL = await contract.methods.tokenURI(tokens[i]).call();
+    const metadataJSON = await fetch('https://cors-anywhere.herokuapp.com/'+metadataURL);
+    const metadata = await metadataJSON.json();
+    const imageURL = metadata.image;
+    nftURLs.push(imageURL);
+  }
+  return nftURLs;
+  // const nftData = await contract.methods.getTokenById(tokenId).call();  
+  // return nftData;  
+}  
+
